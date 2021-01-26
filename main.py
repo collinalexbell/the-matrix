571a2a78bfa81f5766d7bad47e1225bae3484588
kuberlog <collinalexbell@gmail.com>
2021-01-25 20:32:40 -0600

    drawing grid pattern (SUPER SLOW, wtf)

--- main.py
+++ main.py
@@ -14,37 +14,45 @@ ballrect = ball.get_rect()
 
 class View:
     def __init__(self, matrix):
-        self.screen = pygame.display.set_mode((matrix.width, matrix.height))
+        self.pixels_per_m = 10
+
+        screen_size = ( matrix.width * self.pixels_per_m, 
+                matrix.height * self.pixels_per_m)
+
+        self.screen = pygame.display.set_mode(screen_size)
         self.matrix = matrix
+        self.screen.fill(black)
+        self.matrix.draw_background(self.screen, self.pixels_per_m)
 
     def step(self):
-        self.matrix.step(self.screen)
-
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+        self.matrix.draw(self.screen, self.pixels_per_m)
+        pygame.display.flip()
 
 class Matrix:
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.contents = []
+        for i in range(height):
+            self.contents.append([])
+            for i in range(width):
+                self.contents.append(None)
         
-    def step(self, screen):
-        global ball
-        global ballrect
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
+    def draw_background(self, screen, pixels_per_m):
+        for y, row in enumerate(self.contents):
+            for x, column in enumerate(self.contents):
+                rect = pygame.Rect(
+                        (x*pixels_per_m, y*pixels_per_m), 
+                        (pixels_per_m, pixels_per_m))
+                pygame.draw.rect(screen, 111, rect, 1)
 
-        ballrect = ballrect.move(speed)
-        if ballrect.left < 0 or ballrect.right > width:
-            speed[0] = -speed[0]
-        if ballrect.top < 0 or ballrect.bottom > height:
-            speed[1] = -speed[1]
-
-        screen.fill(black)
-        screen.blit(ball, ballrect)
-        pygame.display.flip()
+    def draw(self, screen, pixels_per_unit):
+        pass
 
 
-matrix = Matrix(480,480)
+matrix = Matrix(40,40)
 view = View(matrix)
 while 1:
     view.step()
