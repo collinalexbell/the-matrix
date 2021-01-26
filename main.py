9bafc7b86a780575b40c967b592f707ed47a19fa
kuberlog <collinalexbell@gmail.com>
2021-01-25 20:01:02 -0600

    seperate view from matrix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys, pygame
 import time
+
 pygame.init()
 pygame.font.init()
 
@@ -7,22 +8,45 @@ size = width, height = 320, 240
 speed = [2, 2]
 black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
 
 ball = pygame.image.load("intro_ball.gif")
 ballrect = ball.get_rect()
 
+class View:
+    def __init__(self, matrix):
+        self.screen = pygame.display.set_mode((matrix.width, matrix.height))
+        self.matrix = matrix
+
+    def step(self):
+        self.matrix.step(self.screen)
+
+
+class Matrix:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.contents = []
+        
+    def step(self, screen):
+        global ball
+        global ballrect
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        ballrect = ballrect.move(speed)
+        if ballrect.left < 0 or ballrect.right > width:
+            speed[0] = -speed[0]
+        if ballrect.top < 0 or ballrect.bottom > height:
+            speed[1] = -speed[1]
+
+        screen.fill(black)
+        screen.blit(ball, ballrect)
+        pygame.display.flip()
+
+
+matrix = Matrix(480,480)
+view = View(matrix)
 while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+    view.step()
     time.sleep(0.05)
+
